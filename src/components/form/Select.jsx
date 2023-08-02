f1369c2d839dc52bf23c294925a87c2a08d5928d
unknown <fontes.leonardo@aluno.ifsp.edu.br>
2023-08-01 21:01:31 -0300

    consumndo a api

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -1,13 +1,19 @@
-function Select({ text, name, options,  handleOnChange, value }) {
-    return (
-      <div className="flex flex-col gap-2">
-        <label className="font-bold text-xl text-[#222]" htmlFor={name}>{text}</label>
-        <select className="p-2" name={name} id={name}>
-            <option >Selecione uma opção:</option>
-        </select>
-      </div>
-    );
-  }
-  
-  export default Select;
-  
+function Select({ text, name, options, handleOnChange, value }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <label className="font-bold text-xl text-[#222]" htmlFor={name}>
+        {text}
+      </label>
+      <select className="p-2" name={name} id={name}>
+        <option>Selecione uma opção:</option>
+        {options?.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default Select;
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "content-Type": "application/json",
+      },
+      
+    })
+      .then((resp) => resp.json())
+      .then((categorias) => {
+        setCategories(categorias);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <form
@@ -27,7 +45,11 @@ function ProjectForm({ btnText }) {
           />
         </div>
         <div>
-          <Select name="category_id" text="Selecione a categoria:" />
+          <Select
+            name="category_id"
+            text="Selecione a categoria:"
+            options={categories}
+          />
         </div>
         <div>
           <SubmitButton text={btnText} />
